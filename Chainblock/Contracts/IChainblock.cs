6bf903c668fb27ed494bed56b59e74439ce064e4
Georgi Tsonkov <gtsonkov@abv.bg>
2020-05-02 11:17:13 +0200

    init interfaces

--- Chainblock/Contracts/IChainblock.cs
+++ Chainblock/Contracts/IChainblock.cs
@@ -4,6 +4,38 @@ namespace Chainblock.Contracts
 {
     public interface IChainblock : IEnumerable<ITransaction>
     {
-        
+        int Count { get; }
+
+        void Add(ITransaction tx);
+
+        bool Contains(ITransaction tx);
+
+        bool Contains(int id);
+
+        void ChangeTransactionStatus(int id, TransactionStatus newStatus);
+
+        void RemoveTransactionById(int id);
+
+        ITransaction GetById(int id);
+
+        IEnumerable<ITransaction> GetByTransactionStatus(TransactionStatus status);
+
+        IEnumerable<string> GetAllSendersWithTransactionStatus(TransactionStatus status);
+
+        IEnumerable<string> GetAllReceiversWithTransactionStatus(TransactionStatus status);
+
+        IEnumerable<ITransaction> GetAllOrderedByLenghtDescendingThenById();
+
+        IEnumerable<ITransaction> GetBySenderOrderedByLenghtDescending(string sender);
+
+        IEnumerable<ITransaction> GetByReceiverOrderedByAmountThenById(string receiver);
+
+        IEnumerable<ITransaction> GetByTransactionStatusAndMaximumLenght(TransactionStatus status, int lenght);
+
+        IEnumerable<ITransaction> GetBySenderAndMinimumLenghtDescending(string sender, int lenght);
+
+        IEnumerable<ITransaction> GetByReceiverAndLeghtRange(string receiver, int lo, int hi);
+
+        IEnumerable<ITransaction> GetAllInLenghtRange(int lo, int hi);
     }
 }
--- Chainblock/Contracts/ITransaction.cs
+++ Chainblock/Contracts/ITransaction.cs
@@ -4,6 +4,14 @@ namespace Chainblock.Contracts
 {
     public interface ITransaction
     {
-        
+        int Id { get; set; }
+
+        TransactionStatus Status { get; set; }
+
+        string From { get; set; }
+
+        string To { get; set; }
+
+        string SequreString { get; set; }
     }
 }
